98ab195bd4a6c4360cb40d0b3deb9857a68ae844
Daniel Nguyen <mduongbkhn@gmail.com>
2023-07-28 08:22:31 +0900

    Update env_agent_utils.py

--- envs/env_agent_utils.py
+++ envs/env_agent_utils.py
@@ -39,11 +39,12 @@ class env_agent_utils():
         # p_u  : maximum power of user u
         # butt : local accuracy of users
         # tau  : sampling delay
+        mini_eps = 10e-28
         beta = action[0][0: self.N_User].astype(float)
         beta = scipy.special.softmax(beta, axis=None)
 
         f_u = action[0][self.N_User: 2 * self.N_User].astype(float) * self.f_u_max
-        p_u = (action[0][2 * self.N_User: 3 * self.N_User].astype(float)) * self.p_u_max
+        p_u = (action[0][2 * self.N_User: 3 * self.N_User].astype(float)) * (self.p_u_max-mini_eps) + mini_eps
         butt = (action[0][3 * self.N_User: 3 * self.N_User+1].astype(float))
         # tau = (action[0][3 * self.N_User+1: 3 * self.N_User+2].astype(float)) * 100
         tau = (action[0][3 * self.N_User+1: 3 * self.N_User+2].astype(float)) * (self.skip_max-1) + 1
