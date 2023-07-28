4cf19c3990a29dbf36e01029ac25922909070e16
Daniel Nguyen <mduongbkhn@gmail.com>
2023-07-28 09:30:50 +0900

    Update env_utils.py

--- envs/env_utils.py
+++ envs/env_utils.py
@@ -68,8 +68,6 @@ class env_utils():
         Denominator = self.B * self.beta * self.sigma + mini_eps  # self.B must be a list among all users [1, ... , U]
 
         DataRate = self.B * self.beta * np.log2(1 + (Numerator / Denominator))
-        DataRate[DataRate == 0] = 10e-28
-
         return DataRate
 
     def _calculateGlobalIteration(self):
