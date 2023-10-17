8a386cdad488f549a1f104920040c0d153f5d5fb
Daniel Nguyen <mduongbkhn@gmail.com>
2023-10-18 00:17:25 +0900

    Update agent_sac.py

--- agents/sac/agent_sac.py
+++ agents/sac/agent_sac.py
@@ -189,7 +189,8 @@ class SAC(object):
                     "-" + str(args.user_num) + "-" + str(args.pen_coeff) + \
                     "-" + args.drl_algo + "-" + str(args.batch_size) + \
                     "-" + args.ai_network + "-" + args.algo + "-" + str(args.poweru_max) + \
-                    "-" + str(args.global_acc)
+                    "-" + str(args.global_acc) + "-" + str(args.f_u_max) + "-" + str(args.data_size) + \
+                    "-" + str(args.skip_max)
         """Train the agent."""
         list_results = []
         actor_losses = []
